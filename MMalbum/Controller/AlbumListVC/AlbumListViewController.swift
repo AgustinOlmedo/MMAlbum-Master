bf377008c2771a8468acfd959b335228b7cbd252
Agustin Olmedo <>
2019-11-07 14:11:12 -0300

    Fixed the path of Model folder that were leading to nowhere

--- MMalbum/Controller/AlbumListVC/AlbumListViewController.swift
+++ MMalbum/Controller/AlbumListVC/AlbumListViewController.swift
@@ -3,7 +3,6 @@
 //
 
 import UIKit
-import SwiftyJSON
 import Alamofire
 
 class AlbumListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate {
